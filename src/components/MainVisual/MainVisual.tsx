1b5668688236b9ecbb18bb684addac350fdfb8fd
Haruki <h.haruki0w@gmail.com>
2025-08-08 01:09:30 +0900

    feat: （仮）バトル風に修正

--- src/components/MainVisual/MainVisual.tsx
+++ src/components/MainVisual/MainVisual.tsx
@@ -21,36 +21,49 @@ const MainVisual: React.FC<MainVisualProps> = ({
         {/* バトルフィールド背景 */}
         <div className="battle-background" />
 
-        {/* プレイヤー側（左） */}
-        <div className="player-side">
-          <div className="character-box player-character">
-            <div className="character-emoji">👨‍💻</div>
-          </div>
-          <div className="character-name">Developer L25</div>
-          <div className="hp-bar">
-            <div className="hp-fill player-hp" style={{ width: "85%" }} />
-          </div>
-          <div className="hp-text">85/100</div>
-        </div>
-
-        {/* 敵側（右） */}
-        <div className="enemy-side">
-          <div className="character-box enemy-character">
-            <div className="character-emoji">💼</div>
-          </div>
-          <div className="character-name">Project L15</div>
+        {/* 敵HP情報（左上） */}
+        <div className="enemy-hp-area">
+          <div className="character-name">PROJECT L15</div>
           <div className="hp-bar">
             <div className="hp-fill enemy-hp" style={{ width: "60%" }} />
           </div>
           <div className="hp-text">60/100</div>
         </div>
 
-        {/* 中央のメッセージエリア */}
-        {/* <div className="message-area">
-          <div className="message-text">
-            ポートフォリオを見に来てくれてありがとう！
+        {/* 敵キャラクター（右上） */}
+        <div className="enemy-character-area">
+          <div className="character-box enemy-character">
+            <div className="pixel-character enemy-sprite">
+              <div className="enemy-main"></div>
+              <div className="enemy-detail1"></div>
+              <div className="enemy-detail2"></div>
+              <div className="enemy-detail3"></div>
+            </div>
           </div>
-        </div> */}
+        </div>
+
+        {/* プレイヤーキャラクター（左下） */}
+        <div className="player-character-area">
+          <div className="character-box player-character">
+            <div className="pixel-character player-sprite">
+              <div className="pixel-head"></div>
+              <div className="pixel-body"></div>
+              <div className="pixel-arms"></div>
+              <div className="pixel-legs"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* プレイヤーHP情報（右下） */}
+        <div className="player-hp-area">
+          <div className="character-name">DEVELOPER L25</div>
+          <div className="hp-bar">
+            <div className="hp-fill player-hp" style={{ width: "85%" }} />
+          </div>
+          <div className="hp-text">85/100</div>
+        </div>
+
+        {/* 中央のメッセージエリア */}
       </div>
 
       {/* 下部コマンドエリア */}
